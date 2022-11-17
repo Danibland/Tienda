368533f3e82df87e59802f2d398a46cc920529d9
Your name here <danielblandon_01@hotmail.com>
2022-11-16 19:50:34 -0500

    Actualizacion con envio de correo

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -126,9 +126,9 @@ MEDIA_URL = '/imagenes/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND="django.core,mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
-EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER="d.blandon423@pascualbravo.edi.co"
+EMAIL_HOST_USER="d.blandon423@pascualbravo.edu.co"
 EMAIL_HOST_PASSWORD="3103817974"
+EMAIL_USE_TLS=True
+EMAIL_USE_SSL=False
--- inventario/views.py
+++ inventario/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from inventario.models import productos
 from .forms import productos_form
+from django.views import View 
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
 
-# Create your views here.
+
 def saludo(request):
     return render(request,'paginas/inicio.html')
 
@@ -22,17 +25,33 @@ def crear_producto(request):
     
     return render(request,'productos/crear.html', {'formulario':formulario})
 
+def send_email(nombre, correo, direccion, ciudad, tel):
+    contexto = {'nombre': nombre, 'direccion': direccion, 'ciudad': ciudad, 'tel': tel}
+    
+    template = get_template('correo.html')
+    content = template.render(contexto)
+    
+    email = EmailMultiAlternatives(
+        'Titulo cacorreo',
+        'cuerpo cacorreo',
+        settings.EMAIL_HOST_USER,
+        [correo]
+    )
+    
+    email.attach_alternative(content, 'text/html')
+    email.send()
+    print('envió el cacorreo')
+
 def editar(request):
+    
+    if request.method=='POST':
+        nombre = request.POST.get('nombre')
+        email = request.POST.get('email')
+        direccion = request.POST.get('direccion')
+        ciudad = request.POST.get('ciudad')
+        tel = request.POST.get('tel')
+        send_email(nombre, email, direccion, ciudad, tel)
+    
     return render(request,'productos/editar.html')
 
-"""def editar(request):
-    if request.method=="POST":
-        subject=request.POST['asunto']
-        
-        message=request.POST['mensaje']+ "Remitente "+ request.POST['correo']
-        email_from=settings.EMAIL_HOST_USER
-        recipent_list=[""]
-        send_mail(subject, message, email_from, recipent_list)
-        return redirect('editar.html')
-    
-    return   render(request,"editar.html")  """
+
